0419aeda1ce3b94f1e8f6bf319fa63ee75cefd28
Mohtashim_Butt <87702903+MohtashimButt@users.noreply.github.com>
2023-06-21 17:42:42 +0500

    Add files via upload

---
+++ blahblah.m
@@ -0,0 +1,33 @@
+% Generate data points
+n = 100;  % Number of data points
+x = linspace(-1, 1, n);  % Linearly spaced x-coordinates between -1 and 1
+y = linspace(-1, 1, n);  % Linearly spaced y-coordinates between -1 and 1
+[X, Y] = meshgrid(x, y);  % Grid points for x and y
+
+% Calculate z-coordinates based on a parabolic function
+Z = X.^2 + Y.^2;
+
+% Create a figure window for the plots
+figure;
+
+% Plot the 3D surface plot
+surf(X, Y, Z);
+colorbar;  % Display a color bar
+
+hold on;
+
+% Plot the contour plot
+contour(X, Y, Z, 15, 'LineColor', 'k', 'LineWidth', 1.5);
+
+% Set labels and title for the plots
+xlabel('X-axis');
+ylabel('Y-axis');
+zlabel('Z-axis');
+title('3D Surface Plot with Contour');
+
+while true
+  
+endwhile
+% Set the grid lines on
+grid on;
+
