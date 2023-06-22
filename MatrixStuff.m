8b4440bccc17ea5b3d757673b1ae6188198d1aab
Mohtashim_Butt <87702903+MohtashimButt@users.noreply.github.com>
2023-06-22 16:08:32 +0500

    Add files via upload

---
+++ MatrixStuff.m
@@ -0,0 +1,14 @@
+Matrix1 = [[1,2,3],
+           [4,5,6],
+           [7,8,9]]
+Matrix2 = [[7,8,9],
+           [6,5,4],
+           [3,2,1]]
+%Adding two matrices
+sumOfMatrices = Matrix1 + Matrix2
+%Subtracting two matrices
+subTractionOfMatrices = Matrix1 - Matrix2
+%Multiplying two matrices
+multiplicationOfMatrices = Matrix1 * Matrix2
+%Calculating the Determinant of matrix1
+determinantOfMatrix = det(Matrix2)
