e3b90fb0f6c3a6a1c5cf816243f024bbdb3cdae0
Mohtashim_Butt <87702903+MohtashimButt@users.noreply.github.com>
2023-06-22 17:00:59 +0500

    Update MathematicalOperation.m

--- MathematicalOperation.m
+++ MathematicalOperation.m
@@ -1,6 +1,6 @@
 %code to find average of three numbers
-number1 = 56;
-number2 = 34;
-number3 = 32;
-sum = number1 + number2 + number3;
+number1 = 56
+number2 = 34
+number3 = 32
+sum = number1 + number2 + number3
 avg = sum/3
